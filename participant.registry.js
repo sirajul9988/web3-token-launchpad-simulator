80d6e15afeda92bc4ac27e4d80b94974dbb5e9eb
Sirajul <161524666+sirajul9988@users.noreply.github.com>
2026-01-18 14:58:48 +0600

    Create participant.registry.js

---
+++ participant.registry.js
@@ -0,0 +1,9 @@
+export function loadParticipants() {
+  console.log("Loading launch participants...");
+
+  return [
+    { address: "0xUSER001", contribution: 5 },
+    { address: "0xUSER002", contribution: 10 },
+    { address: "0xUSER003", contribution: 2 }
+  ];
+}
